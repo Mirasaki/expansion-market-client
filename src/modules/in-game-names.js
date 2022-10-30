5658be3e3ba5424f9aa3678f704ea8c475c55b53
Mirasaki <pcr.hillebrand1998@gmail.com>
2022-10-30 17:42:48 +0100

    refactor for reusability

--- src/modules/in-game-names.js
+++ src/modules/in-game-names.js
@@ -1,28 +1,42 @@
+const logger = require('@mirasaki/logger');
 const { default: axios } = require('axios');
 
-const { BACKEND_URL } = process.env;
+const {
+  BACKEND_URL,
+  DEBUG_ITEM_LIST_REQUESTS
+} = process.env;
 
 const getItemList = async (id) => {
+  let clientResponse;
   try {
     const res = await axios({
       method: 'GET',
       url: `${BACKEND_URL}/api/in-game-names/${id}`
     });
-    return {
+    clientResponse = {
       status: res.status,
       statusText: res.statusText,
       data: res.data
     };
   } catch (err) {
-    return {
+    clientResponse = {
       status: err.response.status,
       statusText: err.response.statusText,
       data: err.response.data
     };
   }
+
+  if (DEBUG_ITEM_LIST_REQUESTS === 'true') {
+    logger.startLog('[GET] Item List Request');
+    console.dir(clientResponse, { depth: 1 });
+    logger.endLog('[GET] Item List Request');
+  }
+
+  return clientResponse;
 };
 
 const putItemList = async (id, itemList) => {
+  let clientResponse;
   try {
     const res = await axios({
       method: 'PUT',
@@ -30,18 +44,26 @@ const putItemList = async (id, itemList) => {
       headers: { 'Content-Type': 'application/json' },
       data: { item_list: itemList }
     });
-    return {
+    clientResponse = {
       status: res.status,
       statusText: res.statusText,
       data: res.data
     };
   } catch (err) {
-    return {
+    clientResponse = {
       status: err.response.status,
       statusText: err.response.statusText,
       data: err.response.data
     };
   }
+
+  if (DEBUG_ITEM_LIST_REQUESTS === 'true') {
+    logger.startLog('[PUT] Item List Request');
+    console.dir(clientResponse, { depth: 1 });
+    logger.endLog('[PUT] Item List Request');
+  }
+
+  return clientResponse;
 };
 
 module.exports = {
