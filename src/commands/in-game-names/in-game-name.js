c7e001f8c926fedfb71c0b0019b1e2e7b5267be9
Mirasaki <pcr.hillebrand1998@gmail.com>
2022-10-31 13:39:03 +0100

    add /in-game-name, resolves in-game name for item

--- src/commands/in-game-names/in-game-name.js
+++ src/commands/in-game-names/in-game-name.js
@@ -1,10 +1,18 @@
 const { ApplicationCommandOptionType } = require('discord.js');
 const { ChatInputCommand } = require('../../classes/Commands');
-const { getItemList } = require('../../modules/in-game-names');
+const { fetchInGameName } = require('../../modules/in-game-names');
+const { colorResolver } = require('../../util');
 
 const CLASS_NAME_OPTION_STRING = 'class-name';
+const ERROR_ITEM_UNRESOLVED = 'ItemUnresolved';
+const ERROR_NO_ITEM_LIST = 'NoItemList';
 
 module.exports = new ChatInputCommand({
+  cooldown: {
+    type: 'user',
+    usages: 2,
+    duration: 10
+  },
   data: {
     description: 'Display the in-game name for an item/class name',
     options: [
@@ -21,34 +29,64 @@ module.exports = new ChatInputCommand({
   run: async (client, interaction) => {
     // Destructuring
     const { member, options, guild } = interaction;
-    const { emojis } = client.container;
+    const { emojis, colors } = client.container;
     const query = options.getString(CLASS_NAME_OPTION_STRING);
 
     // Deferring our reply
     await interaction.deferReply();
 
     // Fetching our data
-    const res = await getItemList(guild.id);
+    const res = await fetchInGameName(guild.id, query);
 
     // 200 - OK
     if (res.status === 200) {
-      // Destructuring and finding the configuration for query
+      // Destructuring and user feedback
       const { data } = res;
-      const itemList = Object.entries(data.valid);
-      const itemEntry = itemList.find(([className, inGameName]) => className === query.toLowerCase());
-
-      // Responding appropriately depending on if the item has an
-      // in-game name configured
-      if (!itemEntry) interaction.editReply(`${emojis.error} ${member}, I can't find the class **\`${query}\`** in this server's in-game names configuration`);
-      else interaction.editReply(`${emojis.success} ${member}, **\`${query}\`**'s in-game name is: **${itemEntry[1]}**`);
+      const classNameEndsWithS = data.charAt(data.length - 1) === 's';
+      interaction.editReply(`${emojis.success} ${member}, **\`${query}\`**'${classNameEndsWithS ? '' : 's'} in-game name is: **${data}**`);
     }
 
     // Not Found
     else if (res.status === 404) {
-      interaction.editReply(`${emojis.error} ${member}, couldn't find an in-games name configuration for this server. Use **/set-item-list** first.`);
+      const { error, message } = res;
+
+      // No in-game name for class/item
+      if (error === ERROR_ITEM_UNRESOLVED) {
+        interaction.editReply({
+          content: `${emojis.error} ${member}, couldn't resolve the in-game name`,
+          embeds: [{
+            color: colorResolver(colors.warning),
+            title: message,
+            description: `\`${query}\` missing from /set-item-list configuration`
+          }]
+        });
+      }
+
+      // No item list configured for guild
+      else if (error === ERROR_NO_ITEM_LIST) {
+        interaction.editReply({
+          content: `${emojis.error} ${member}, couldn't resolve the in-game name`,
+          embeds: [{
+            color: colorResolver(colors.error),
+            title: message,
+            description: 'Use **`/set-item-list`** before you can use this command to resolve in-game item names'
+          }]
+        });
+      }
     }
 
     // Unknown error
-    else interaction.editReply(`${emojis.error} ${member}, unexpected error encountered: ${res.code} | ${res.statusText}`);
+    else {
+      const { status, statusText, error, message } = res;
+      interaction.editReply({
+        content: `${emojis.error} ${member}, couldn't resolve the in-game name`,
+        embeds: [{
+          color: colorResolver(colors.error),
+          title: error || 'Unexpected error',
+          description: message,
+          footer: { text: `${status} | ${statusText}` }
+        }]
+      });
+    }
   }
 });
