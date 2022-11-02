f69acbb098c33b2beb64c1b70f041e9d58001c34
Mirasaki <pcr.hillebrand1998@gmail.com>
2022-11-02 21:38:58 +0100

    add /set-market-traders command

---
+++ src/commands/market-traders/set-market-traders.js
@@ -0,0 +1,152 @@
+const { ApplicationCommandOptionType, AttachmentBuilder } = require('discord.js');
+const { ChatInputCommand } = require('../../classes/Commands');
+const { isAllowedContentType, fetchAttachment, colorResolver, getRelativeTime, getRuntime } = require('../../util');
+const { stripIndents } = require('common-tags');
+const BackendClient = require('../../lib/client');
+const { putMarketTraders } = require('../../lib/requests.js');
+
+// Constants & Helpers
+const {
+  MARKET_TRADERS_FILE_DESCRIPTION,
+  MARKET_TRADERS_OPTION_NAME,
+  MARKET_TRADERS_REAL_FILE_NAME
+} = require('../../constants');
+const { bulkResolveInGameNames, matchResolvedInGameNameArray } = require('../../lib/helpers/in-game-names');
+const { getAllCurrencies, formatAmountInCurrency } = require('../../lib/helpers/market-traders');
+
+const ALLOWED_CONTENT_TYPE = 'application/zip';
+
+module.exports = new ChatInputCommand({
+  permLevel: 'Administrator',
+  cooldown: {
+    type: 'guild',
+    usages: 1,
+    duration: 60
+  },
+  data: {
+    description: `Upload your server's ${MARKET_TRADERS_FILE_DESCRIPTION}`,
+    options: [
+      {
+        type: ApplicationCommandOptionType.Attachment,
+        name: MARKET_TRADERS_OPTION_NAME,
+        description: `Your "${MARKET_TRADERS_REAL_FILE_NAME}" file.`,
+        required: true
+      }
+    ]
+  },
+
+  run: async (client, interaction) => {
+    // Destructuring
+    const { member, guild } = interaction;
+    const { emojis } = client.container;
+
+    // Deferring our reply
+    await interaction.deferReply();
+
+    // Assign user's attachment
+    const attachment = interaction.options.getAttachment(MARKET_TRADERS_OPTION_NAME);
+
+    // Return if content type is not allowed
+    const contentIsAllowed = isAllowedContentType(ALLOWED_CONTENT_TYPE, attachment.contentType);
+    if (!contentIsAllowed.strict) {
+      interaction.editReply({
+        content: `${emojis.error} ${member}, file rejected. Content type is not **\`${ALLOWED_CONTENT_TYPE}\`**, received **\`${attachment.contentType}\`** instead.`
+      });
+      return;
+    }
+
+    // User Feedback, wait for parser
+    let content = `${emojis.wait} ${member}, please be patient while your \`${MARKET_TRADERS_OPTION_NAME}\` attachment is being retrieved...`;
+    await interaction.editReply(content);
+
+    // Fetch the attachment from Discord's API
+    const attachmentResponse = await fetchAttachment(attachment);
+
+    // Return if any errors were encountered
+    if ('error' in attachmentResponse) {
+      content += `\n${emojis.error} Couldn't retrieve attachment, this command has been cancelled`;
+      interaction.editReply({
+        content,
+        embeds: [BackendClient.getClientErrorEmbed(attachmentResponse)]
+      });
+      return;
+    }
+
+    // Valid data was received
+
+    // Notify attachment has been fetched
+    const { runtime, size, body } = attachmentResponse;
+    content += `\n${emojis.success} Fetched your attachment in: **${runtime} ms** (${size} KB)`;
+    await interaction.editReply(content);
+
+    // Notify start API parser
+    content += `\n${emojis.wait} Parsing and saving your ${MARKET_TRADERS_FILE_DESCRIPTION}...`;
+    await interaction.editReply(content);
+
+    // Response from API
+    const requestTimerStart = process.hrtime.bigint();
+    const res = await putMarketTraders(guild.id, body);
+    const requestFetchMS = getRuntime(requestTimerStart).ms;
+
+    // Error embed if the request isn't successful
+    if (res.status !== 200) {
+      content += `\n${emojis.error} This file couldn't be parsed/processed`;
+      interaction.editReply({
+        content,
+        embeds: [BackendClient.getClientErrorEmbed(res)]
+      });
+    }
+
+
+    // 200 - OK - Success
+    else {
+      const { data } = res;
+
+      // Calculate totals
+      const totalCategories = data.reduce(
+        (accumulator, currValue) => accumulator += currValue.categories.length, 0 // Initial accumulator
+      );
+      const totalItems = data.reduce(
+        (accumulator, currValue) => accumulator += Object.values(currValue.items).length, 0
+      );
+
+      // Resolving currencies
+      const uniqueCurrenciesUsed = getAllCurrencies(data); // OR getAllLowestCurrencies(data)
+      const currenciesInGameNames = await bulkResolveInGameNames(guild.id, uniqueCurrenciesUsed);
+      const resolvedCurrencyArray = matchResolvedInGameNameArray(uniqueCurrenciesUsed, currenciesInGameNames);
+      const formattedCurrencies = formatAmountInCurrency(resolvedCurrencyArray);
+
+      // Legacy code
+      // Old approach, with endpoint that only resolves 1 item
+      // const currenciesInGameNames = await Promise.all(uniqueCurrenciesUsed.map((curr) => resolveInGameName(guild.id, curr)));
+
+      // Replying to the interaction
+      content += `\n${emojis.success} Finished parsing and saving your ${MARKET_TRADERS_FILE_DESCRIPTION} in: **${requestFetchMS} ms**`;
+      interaction.editReply({
+        content,
+        embeds: [{
+          color: colorResolver(),
+          title: `${MARKET_TRADERS_FILE_DESCRIPTION} for: ${guild.name}`,
+          description: stripIndents`
+            **Parsed Traders:** ${data.length}
+            **Categories Configured:** ${totalCategories}
+            **Trader-Specific Items Configured:** ${totalItems}
+
+            **Currencies Used:**
+            ${formattedCurrencies.join('\n')}
+          `,
+          footer: {
+            text: stripIndents`
+              Updated: ${getRelativeTime(data[0].updatedAt)}
+              Created: ${getRelativeTime(data[0].createdAt)}
+            `
+          }
+        }],
+        files: [
+          new AttachmentBuilder(Buffer.from(JSON.stringify(data, null, 2)))
+            .setName(`${MARKET_TRADERS_REAL_FILE_NAME}-parsed.json`)
+        ]
+      });
+    }
+  }
+});
