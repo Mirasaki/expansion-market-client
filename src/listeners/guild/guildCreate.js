5e54a47e0c3e7b4e9a04139dadb24afca7b847f3
Mirasaki <pcr.hillebrand1998@gmail.com>
2023-02-23 17:14:24 +0100

    Guild join messages in my server, pog!

--- src/listeners/guild/guildCreate.js
+++ src/listeners/guild/guildCreate.js
@@ -1,5 +1,11 @@
 const logger = require('@mirasaki/logger');
 const chalk = require('chalk');
+const { parseSnakeCaseArray, getRelativeTime } = require('../../util');
+
+const {
+  NODE_ENV,
+  GUILD_JOIN_LEAVE_CHANNEL_ID
+} = process.env;
 
 module.exports = (client, guild) => {
   // Always check to make sure the guild is available
@@ -7,4 +13,46 @@ module.exports = (client, guild) => {
 
   // Logging the event to our console
   logger.success(`${chalk.greenBright('[GUILD JOIN]')} ${guild.name} has added the bot! Members: ${guild.memberCount}`);
+
+  // Send Server join/leave messages in production
+  if (NODE_ENV === 'production') {
+    const guildJoinLeaveChannel = client.channels.cache.get(GUILD_JOIN_LEAVE_CHANNEL_ID);
+    try {
+      guildJoinLeaveChannel?.send({
+        content: `**__${guild.name}__** has added <@!${client.user.id}> to their server!`,
+        embeds: [{
+          color: 6618980,
+          title: `[${guild.preferredLocale}]: ${guild.name}`,
+          description: guild.description || null,
+          thumbnail: { url: guild.iconURL({ dynamic: true }) },
+          image: { url: guild.bannerURL() },
+          fields: [
+            {
+              name: 'Members',
+              value: `${(guild.memberCount).toLocaleString('en-US')}`,
+              inline: true
+            },
+            {
+              name: 'Discord Boosts',
+              value: `Tier ${guild.premiumTier} @ ${guild.premiumSubscriptionCount} boosts`,
+              inline: true
+            },
+            {
+              name: 'Features',
+              value: `${parseSnakeCaseArray(guild.features) || 'None!'}`,
+              inline: false
+            },
+            {
+              name: 'Created',
+              value: `${new Date(guild.createdAt).toLocaleString()}\n${getRelativeTime(guild.createdAt)}`,
+              inline: true
+            }
+          ]
+        }]
+      });
+    } catch (err) {
+      logger.syserr(`Error while trying to send guildJoin message in #${guildJoinLeaveChannel.name}`);
+      console.error(err);
+    }
+  }
 };
